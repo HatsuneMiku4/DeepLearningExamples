f9d08de96a86f92f04efebb7c9a1d86651339b3c
ZKong <kong.zhe@husky.neu.edu>
2020-01-08 21:03:41 -0500

    load_full_checkpoint

--- PyTorch/LanguageModeling/BERT/run_pretraining_admm.py
+++ PyTorch/LanguageModeling/BERT/run_pretraining_admm.py
@@ -196,10 +196,8 @@ class CheckpointMixin:
         self._set_timer_status(state_dict['timer'])
         if self.cur_phase == PruningPhase.admm:
             cur_rho = self.cur_rho
-            cur_lambda = None
-            for rho_idx in count():
-                if cur_rho <= self._calc_current_rho(rho_idx):
-                    cur_lambda = self._calc_current_lamda(rho_idx)
+            init_rho_idx = [self._calc_current_rho(i) for i in range(self.rho_num)].index(self.cur_rho)
+            cur_lambda = self._calc_current_lamda(rho_idx)
         else:
             cur_rho = self.initial_rho
             cur_lambda = self.initial_lambda
